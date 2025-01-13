75af427ba88b65b25396ce85058cbc8df54545cc
Josh K <linkspace003@fastmail.com>
2025-01-13 15:21:30 -0500

    Updated: 2025-01-13 15:21:30

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,18 +1,15 @@
-require("config.lazy")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-local set = vim.opt
-
-set.shiftwidth = 4 -- tab = 'x' number of spaces
-set.number = true -- shows line numbers
-set.clipboard = "unnamedplus" -- use clipboard buffer from operating system
-
-
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>", {desc = "source file"})
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-    callback = function()
-	vim.highlight.on_yank()
-    end,
-})
+require("vim-options")
+require("lazy").setup("plugins")
---
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,100 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗",
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
+            ensure_installed = {
+                "cssls",
+                "eslint",
+                "html",
+                "jsonls",
+                "lua_ls",
+                "pyright",
+                "tsserver",
+            }
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        opts = {},
+        config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            local lspconfig = require("lspconfig")
+            lspconfig.cssls.setup({ capabilities = capabilities })
+            lspconfig.eslint.setup({ capabilities = capabilities })
+            lspconfig.html.setup({
+                capabilities = capabilities
+            })
+            lspconfig.jsonls.setup({ capabilities = capabilities })
+            lspconfig.lua_ls.setup({
+                settings = {
+                    Lua = {
+                        runtime = {
+                            -- Tell the language server which version of Lua you're using
+                            -- (most likely LuaJIT in the case of Neovim)
+                            version = 'LuaJIT',
+                        },
+                        diagnostics = {
+                            -- Get the language server to recognize the `vim` global
+                            globals = {
+                                'vim',
+                                'require'
+                            },
+                        },
+                        workspace = {
+                            -- Make the server aware of Neovim runtime files
+                            library = vim.api.nvim_get_runtime_file("", true),
+                        },
+                        -- Do not send telemetry data containing a randomized but unique identifier
+                        telemetry = {
+                            enable = false,
+                        },
+                    },
+                },
+                capabilities = capabilities
+            })
+            lspconfig.pyright.setup({ capabilities = capabilities })
+            lspconfig.tsserver.setup({
+                capabilities = capabilities
+            })
+
+            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = '[R]e[n]ame' })
+            vim.keymap.set('n', '<leader>ca', function()
+                vim.lsp.buf.code_action { context = { only = { 'quickfix', 'refactor', 'source' } } }
+            end, { desc = '[C]ode [A]ction' })
+
+            vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, { desc = '[G]oto [D]efinition' })
+            vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { desc = '[G]oto [R]eferences' })
+            vim.keymap.set('n', 'gI', require('telescope.builtin').lsp_implementations,
+                { desc = '[G]oto [I]mplementation' })
+            vim.keymap.set('n', '<leader>D', require('telescope.builtin').lsp_type_definitions,
+                { desc = 'Type [D]efinition' })
+            vim.keymap.set('n', '<leader>sd', require('telescope.builtin').lsp_document_symbols,
+                { desc = '[S]how [D]ocument Symbols (Functions, Variables, etc...)' })
+            vim.keymap.set('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
+                { desc = '[W]orkspace [S]ymbols' })
+
+            -- See `:help K` for why this keymap
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Hover Documentation' })
+            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { desc = 'Signature Documentation' })
+        end
+    }
+}
