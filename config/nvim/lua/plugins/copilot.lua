2520c2ef78d0d05f1ee5b998bff4845e597abb30
Joshua Kagiwada <jkagiwada@advisor360.com>
2025-01-28 09:27:27 -0500

    Update dot files - 2025-01-28_09:26:30

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -3,6 +3,6 @@ return {
 	config = function()
 		-- GitHub Copilot configuration
 		vim.g.copilot_no_tab_map = true
-		vim.api.nvim_set_keymap("i", "<C-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true, script = true })
+		vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true, script = true })
 	end,
 }
